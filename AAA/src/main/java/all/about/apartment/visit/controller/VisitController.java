9b267099c2dd6433a6d99c3283d174916ec56408
kosta <kosta@kosta-PC>
2017-11-28 11:14:15 +0900

    민식 - 11/28 11: 14

--- AAA/src/main/java/all/about/apartment/visit/controller/VisitController.java
+++ AAA/src/main/java/all/about/apartment/visit/controller/VisitController.java
@@ -1,14 +1,20 @@
 package all.about.apartment.visit.controller;
 
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.inject.Inject;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +25,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import all.about.apartment.publicDomain.ResidentVO;
 import all.about.apartment.visit.domain.Criteria;
 import all.about.apartment.visit.domain.PageMaker;
+import all.about.apartment.visit.domain.ApplicationVO;
 import all.about.apartment.visit.dto.ApplicationDTO;
 import all.about.apartment.visit.service.VisitService;
 @Controller
@@ -89,31 +96,47 @@ public class VisitController {
 		return "/visit/applicationList";
 	}
 
-	@RequestMapping(value = "/visitManagement" , method = RequestMethod.GET)
-	public ModelAndView adminVisitManagement() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("/visit/admin/visitManagement");
-		
-		modelAndView.addObject("application", service.adminApplicationList());
-		
-		return modelAndView;
+	
+	
+	@RequestMapping(value = "/checkUpdate", method = RequestMethod.POST)
+	@ResponseBody
+	public String checkUpdate(@RequestParam(value = "vr_id", required = false) String vr_id,
+			@RequestParam(value = "check", required = false) String check) throws Exception {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		System.out.println("vr_id : " + vr_id + " " + "check : " + check);
+		if (check.equals("O")) {
+			map.put("vr_id", vr_id);
+			map.put("check", check);
+			service.updateCheck(map);
+		} 
+
+		return "/visit/visitManagement";
+	}
+
+	@RequestMapping(value = "/visitManagement")
+	public String adminVisitManagement() throws Exception {
+		System.out.println();
+		return "/visit/admin/visitManagement";
 	}
 	
-	@RequestMapping(value = "/visitCheck", method = RequestMethod.GET)
 	@ResponseBody
-	public String visitCheck(@RequestParam(value = "vr_id", required = false) String vr_id,
-			@RequestParam(value = "state", required = false) String state) throws Exception {
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		System.out.println("vr_id : " + vr_id + " " + "state : " + state);
-		if (state.equals("attendance")) {
-			// visit_re에서 
-		} else {
-			map.put("vr_id", vr_id);
-			map.put("state", "거부");
-		}
-		service.updateCheck(map);
-
-		return "/visit/applicationList";
+	@RequestMapping(value="/searchApplication", method=RequestMethod.POST)
+	public ResponseEntity<List<ApplicationVO>> searchApplication (@RequestBody String selectedDate, Model model) throws Exception {
+		ResponseEntity<List<ApplicationVO>> entity = null;
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("visit/admin/visitManagement");
+		List<ApplicationVO> list = new ArrayList<>();
+		list = service.adminApplicationList(selectedDate);
+		entity = new ResponseEntity<List<ApplicationVO>>(list, HttpStatus.OK);
+		selectedDate = selectedDate.replace("-", "/");
+		selectedDate = selectedDate.substring(2);
+		System.out.println(selectedDate);
+		System.out.println(list);
+		
+		//mav.addObject("applicationList", list);
+		model.addAttribute("applicationList", list);
+		return entity;
 	}
 
 }
