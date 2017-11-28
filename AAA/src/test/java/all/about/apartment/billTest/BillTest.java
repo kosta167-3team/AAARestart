89bdccaf371c0d87830c062e2f25f1d43a0045d3
jeong-gi <jeong-gi@DESKTOP-GPPUBO3>
2017-11-28 17:23:22 +0900

    [정기] JunitTest [17/11/28 17:23]

--- AAA/src/test/java/all/about/apartment/billTest/BillTest.java
+++ AAA/src/test/java/all/about/apartment/billTest/BillTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.annotation.ApplicationScope;
 
+import all.about.apartment.bill.domain.SetEnergyAVG;
+import all.about.apartment.bill.domain.SetMonthBill;
 import all.about.apartment.bill.persistence.BillDAO;
 
 
@@ -22,16 +24,45 @@ public class BillTest {
 	@Inject
 	BillDAO dao;
 	
-
+	SetMonthBill setMonthBill = new SetMonthBill("alstlr123", "2017-11");
 	
-/*	@Test
+	@Test
 	public void daoTest() throws Exception{
 		System.out.println("haha");
 		System.out.println(dao.getPersonal_mgmt_exList());
-	}*/
+	}
 	@Test
 	public void daoEnergyAVG() throws Exception{
 		System.out.println();
+	}
+	@Test
+	public void daoPersonal_mgmt_exListTest() throws Exception{
+		System.out.println(dao.getPersonal_mgmt_exList());
+	}
+	@Test
+	public void monthBillDAOTest()throws Exception{
+		dao.monthBill(new SetMonthBill("alstlr123", "2017-11"));
+	}
+	
+	@Test
+	public void getSaleBillDAOTest() throws Exception{
+		dao.getSaleBill(11);
+	}
+	@Test
+	public void getEnergyList() throws Exception{
+		dao.getEnergyList(new SetMonthBill("alstlr123", "2017-11"));
+	}
+	@Test
+	public void getSelectDetail() throws Exception{
+		dao.getSelectDetail(setMonthBill);
+	}
+	@Test
+	public void getYearEnergy() throws Exception{
+		dao.getYearEnergy(setMonthBill);
+	}
+	@Test
+	public void getElecAVG() throws Exception{
+		dao.getElecAVG(new SetEnergyAVG("elec", "2017-11", 33));
 	}
 	
 	
